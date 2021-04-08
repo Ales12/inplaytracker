2cd13ef621b49f068f9029cd7c9d7b3b2c45c14e
Alexandra <38242434+Ales12@users.noreply.github.com>
2021-04-08 21:35:45 +0200

    Add files via upload

---
+++ inc/languages/deutsch_du/iptracker.lang.php
@@ -0,0 +1,14 @@
+<?php
+$l['iptracker'] = "Deine Inplayszenen";
+$l['iptracker_partner'] = "Teilnehmende Charaktere:";
+$l['iptracker_partner_edit'] = "Möchtest du nachträglich Charaktere hinzufügen, kannst du sie wie folgt hinzufügen: <b>,Username</b>. <br /> Bitte achte auf die korrekte Schreibweise des Charakteres.";
+$l['iptracker_time'] = "Datum &amp; Uhrzeit:";
+$l['iptracker_global'] = "Szenen";
+$l['iptracker_misc'] = "Deine Inplayszenen";
+$l['iptracker_misc_status'] = "Status";
+$l['iptracker_misc_infos'] = "Szeneninformationen";
+$l['iptracker_misc_lastpost'] = "Letzter Post";
+$l['iptracker_pn_usercp'] = "Eine PN bei neuen Post?";
+$l['iptracker_pn_usercp_all'] = "Pns für alle Charaktere aktiveren/deaktivieren?";
+$l['iptracker_profile_active'] = "Aktive Szenen";
+$l['iptracker_profile_end'] = "Beendete Szenen";
